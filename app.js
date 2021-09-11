1c74c13272c1d3d6efb00e46c2cda4bf6e63770f
Giancarlort <stegian.man@gmail.com>
2021-09-11 17:26:22 -0500

    Primer commit API Rest con Express

---
+++ app.js
@@ -0,0 +1,122 @@
+const debug = require('debug')('app:inicio');
+// const dbDebug = require('debug')('app:db');
+
+const express = require('express');
+
+const config = require('config');
+
+//const logger = require('./logger');
+const Joi = require('joi');
+const app = express();
+
+// Configuración de enternos
+console.log("Aplicación: " + config.get('nombre'));
+console.log("DB Server: " + config.get('ConfigDB.host'));
+
+var morgan = require('morgan')
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
+
+if (app.get('env') === "development") {
+    app.use(morgan('tiny'));
+    // console.log('Morgan Habilitado...');
+    debug('Morgan está habilitado');
+}
+
+// Trabajos con la DB
+debug('Conectando con la BD');
+
+//app.use(logger);
+
+const usuarios = [
+    {id:1, nombre:'Giancarlo'},
+    {id:2, nombre:'Diana'},
+    {id:3, nombre:'Rihanna'}
+];
+
+app.get('/', (req, res) => {
+    res.send('Hola Mundo desde Express.');
+});
+
+app.get('/api/usuarios', (req, res) => {
+    res.send(usuarios);
+});
+
+app.get('/api/usuarios/:id',(req, res) => {
+    let usuario = existeUsuario(req.params.id);
+    if(!usuario) res.status(404).send('El usuario no fue encontrado');
+    res.send(usuario);
+});
+
+app.post('/api/usuarios', (req, res) => {
+
+    const schema = Joi.object({
+        nombre: Joi.string().min(3).required()
+    });
+    const {error, value} = validarUsuario(req.body.nombre);
+    if(!error){
+        const usuario = {
+            id: usuarios.length + 1,
+            nombre: value.nombre
+        };
+        usuarios.push(usuario);
+        res.send(usuario);
+    }else{
+        const mensaje = error.details[0].message;
+        res.status(400).send(mensaje);
+    }   
+        
+});
+
+app.put('/api/usuarios/:id', (req, res) => {
+  
+    let usuario = existeUsuario(req.params.id);
+    if(!usuario){
+        res.status(404).send('El usuario no fue encontrado');
+        return;
+    }    
+    
+    const {error, value} = validarUsuario(req.body.nombre);    
+    if(error){
+        const mensaje = error.details[0].message;
+        res.status(400).send(mensaje);
+        return;
+    }
+
+    usuario.nombre = value.nombre;
+    res.send(usuario);
+});
+
+app.delete('/api/usuarios/:id', (req, res) => {
+    let usuario = existeUsuario(req.params.id);
+    if(!usuario){
+        res.status(404).send('El usuario no fue encontrado');
+        return;
+    }
+
+    const index = usuarios.indexOf(usuario);
+    usuarios.splice(index, 1);
+
+    res.send(usuario);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Escuchando en el puerto ${port}...`);
+})
+
+function existeUsuario(id){
+    return(usuarios.find(u => u.id === parseInt(id)));
+}
+
+function validarUsuario(nom){
+    const schema = Joi.object({
+        nombre: Joi.string().min(3).required()
+    });
+    return (schema.validate({ nombre: nom }));
+}
+
+
+
